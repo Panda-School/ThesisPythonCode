abb6e95bc0a5308f24c812ca6fd4926e31505a6a
Bas van 't Hazeveld <basvthazeveld@outlook.com>
2025-05-15 15:52:26 +0000

    changed setup

--- setup.sh
+++ setup.sh
@@ -7,5 +7,9 @@ tar -xzf flickr30k_images.tar.gz
 
 mkdir output
 
+wget https://github.com/cli/cli/releases/download/v2.72.0/gh_2.72.0_linux_amd64.deb
+dpkg -i gh_2.72.0_linux_amd64.deb
+gh auth login
+
 git config --global user.email "basvthazeveld@outlook.com"
 git config --global user.name "Bas van 't Hazeveld"
